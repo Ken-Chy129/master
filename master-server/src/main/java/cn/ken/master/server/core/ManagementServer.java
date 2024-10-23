e2c3d40a31a8e2cb452914451d18167881564dc4
Ken-Chy129 <ken-chy129@qq.com>
2024-10-24 01:41:17 +0800

    完善Management服务

--- master-server/src/main/java/cn/ken/master/server/core/ManagementServer.java
+++ master-server/src/main/java/cn/ken/master/server/core/ManagementServer.java
@@ -9,9 +9,7 @@ import cn.ken.master.server.service.FieldService;
 import cn.ken.master.server.utils.ApplicationContextUtil;
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,35 +27,35 @@ public class ManagementServer extends Thread {
             AppService appService = ApplicationContextUtil.getBean(AppService.class);
             FieldService fieldService = ApplicationContextUtil.getBean(FieldService.class);
             Builder.OfVirtual appStartVTB = Thread.ofVirtual().name("AppStart-Thread");
-            log.info("ManagementServer启动成功");
+            log.info("ManagementServer启动成功，端口号:{}, ip地址:{}", serverSocket.getLocalPort(), serverSocket.getLocalSocketAddress());
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    appStartVTB.start(() -> {
-                        PrintWriter pw = null;
-                        try {
-                            //todo:新增事务处理
-                            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                            pw = new PrintWriter(socket.getOutputStream(), true);
-                            // 1.应用启动
-                            RegisterRequest request = (RegisterRequest) ois.readObject();
-                            Long appId = request.getAppId();
-                            String accessKey = request.getAccessKey();
-                            String ipAddress = socket.getInetAddress().getHostAddress();
-                            Integer port = request.getPort();
-                            Result<Boolean> result1 = appService.startAppOnMachine(appId, accessKey, ipAddress, port);
-                            if (!result1.getSuccess()) {
-                                pw.print(result1);
-                            }
-                            // 2.解析受管控字段
-                            Result<Boolean> result2 = fieldService.registerField(appId, request.getNamespaceList());
-                            pw.println(result2);
-                        } catch (Exception e) {
-                            if (pw != null) {
-                                pw.println(Result.error(e.getMessage()));
-                            }
-                        }
-                    });
-                }
+                Socket socket = serverSocket.accept();
+                appStartVTB.start(() -> {
+                    try (
+                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter out = new PrintWriter(socket.getOutputStream(), true)
+                    ) {
+                        log.info("应用连接|host:{},port:{}", socket.getInetAddress().getHostAddress(), socket.getPort());
+                        //todo:新增事务处理
+                        // 1.应用启动
+                        String request = in.readLine();
+                        log.info(request.toString());
+//
+//                            Long appId = request.getAppId();
+//                            String accessKey = request.getAccessKey();
+//                            String ipAddress = socket.getInetAddress().getHostAddress();
+//                            Integer port = request.getPort();
+//                            Result<Boolean> result1 = appService.startAppOnMachine(appId, accessKey, ipAddress, port);
+//                            if (!result1.getSuccess()) {
+//                                pw.print(result1);
+//                            }
+//                            // 2.解析受管控字段
+//                            Result<Boolean> result2 = fieldService.registerField(appId, request.getNamespaceList());
+//                            pw.println(result2);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
